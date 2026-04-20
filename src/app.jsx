f1124ca8276c27ce8b637f09b558eeb8272be320
Jordan <garciatorresjordandavid23@gmail.com>
2026-04-20 15:07:45 -0500

    Jin_Mori

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import {Reloj} from './useeffect'
-
+import {AnimatedCard} from './animacion'
 
 
 
@@ -9,6 +9,7 @@ export function App (){
     <aside>
 
     <Reloj/> 
+    <AnimatedCard/>
     </aside>
 </article>
    )
