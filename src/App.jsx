ef2d60622bf4781bba89c2d9401dffe66f84310a
Jordan <garciatorresjordandavid23@gmail.com>
2026-04-21 15:33:59 -0500

    actividad #1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
 
       <div>
-        <h1>Vite + React</h1>
+        
       </div>
   )
 }
---
+++ src/funciones/filtros.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function Filtros
+() {
+
+const [filtra,setFiltar] = useState();
+
+
+
+  return (
+    <div>
+        <form>
+        <input type="text" />
+        </form>
+    </div>
+  )
+}
